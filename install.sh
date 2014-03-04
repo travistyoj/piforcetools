9ee9f49b3e3f4b88e45bd922c7a2bcc13f5808f1
Travis Brown <tbrown@emeals.com>
2014-03-04 13:42:57 -0500

    Updated install script to enable I2C modules.

--- install.sh
+++ install.sh
@@ -1,22 +1,26 @@
 #!/bin/sh
-echo Creating roms directory..
+echo "Enabling I2C modules..."
+echo "i2c-bcm2708" >> /etc/modules
+echo "i2c-dev" >> /etc/modules
+
+echo "Creating roms directory.."
 mkdir /home/pi/roms
 chown pi:pi /home/pi/roms
 
-echo Copying configuration scripts...
+echo "Copying configuration scripts..."
 cp piforcetools.sh /etc/init.d/
 cp interfaces.* /etc/network/
 
-echo Installing Piforce Tools as daemon...
+echo "Installing Piforce Tools as daemon..."
 cd /etc/init.d
 chown root:root piforcetools.sh
 chmod 755 piforcetools.sh
 update-rc.d piforcetools.sh defaults
 
-echo Configuring network...
+echo "Configuring network..."
 cd /etc/network
 chown root:root interfaces.*
 chmod 755 interfaces.*
 cp interfaces.static interfaces
 
-echo Done! When you reboot, this host will be static configured to 192.168.1.1
+echo "Done! When you reboot, this host will be static configured to 192.168.1.1."
